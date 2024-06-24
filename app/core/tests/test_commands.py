ce3cc38bbd6e116d108817ade97fa8af2f1dfd72
Bruno Gallo <brunogalloartucio@gmail.com>
2024-06-24 14:51:16 -0300

    Added wait_for_db command

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,39 @@
+"""Test custom Django management commands."""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+# Mocks check method that checks database status to simulate the response.
+# We add the patch here so that it applies to all of the class's methods.
+# Adding this patch means all tests will receive an additional param
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database is ready"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationError"""
+        # Returns Psycopg2Error twice, then OperationalError trhee times,
+        # and finally True
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
